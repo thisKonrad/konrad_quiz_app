f7f182029a085b3b1b16f584e4cac74a2a2eb2b7
this.Konrad <konradlebt@gmail.com>
2023-11-02 18:03:48 +0100

    js switch dark mode function

--- screen_mode.js
+++ screen_mode.js
@@ -1,13 +1,14 @@
 /* dark light mode js */
 
+const userImage = document.querySelector('.user_image');
 const htmlPage = document.querySelector('html');
 const toggle = document.querySelector('#dark_toggle');
 
 toggle.addEventListener('click', switchScreenMode);
 
-function switchScreenMode(e){
+function switchScreenMode(){
 
-    console.log('html: ', htmlPage)
+    console.log('html: ', htmlPage);
 
     console.log('switched');
 
@@ -20,6 +21,16 @@ function switchScreenMode(e){
 
         return htmlPage.dataset.theme = 'light';
     
-    } 
+    }
+
+};
+
+if ( htmlPage.dataset.theme === 'dark') {
+
+    userImage.src = './assets/robot_profile_dark_mode.jpg';
+}
+if( htmlPage.dataset.theme === 'light') {
+
+    userImage.src = './assets/robot_profile.jpg';
 
 };
