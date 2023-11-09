9e81240bbb6bb9bef5d9c73fb481e2718590b180
this.Konrad <konradlebt@gmail.com>
2023-11-09 16:09:19 +0100

    add local storage properities for multi page dark mode

--- screen_mode.js
+++ screen_mode.js
@@ -4,8 +4,12 @@ const userImage = document.querySelector('.user_image');
 const htmlPage = document.querySelector('html');
 const toggleSwitch = document.querySelector('#dark_toggle');
 
-let darkMode = localStorage.getItem("dark-mode");
+/* localStorage.setItem('darkMode', 
+JSON.stringify(htmlPage.dataset.theme="dark")); */
 
+
+
+const isDarkMode = htmlPage.dataset.theme="dark";
 
 toggleSwitch.addEventListener("change", function() {
 
@@ -13,8 +17,34 @@ toggleSwitch.addEventListener("change", function() {
         htmlPage.dataset.theme="dark";
         userImage.src='./assets/robot_profile_dark_mode.jpg';
 
+        localStorage.setItem('darkMode', isDarkMode);
+
     } else {
         htmlPage.dataset.theme="light";
         userImage.src='./assets/robot_profile.jpg';
+
+        //localStorage.setItem('lightMode', lightMode);
     }
 });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    
+    localStorage.getItem('darkMode', isDarkMode);
+
+    // Apply dark mode if it's enabled in local storage
+    if ( isDarkMode === 'true') {
+        htmlPage.dataset.theme="dark";
+    }
+    else {
+        htmlPage.dataset.theme="light";
+    }
+});
+
+
+
+/* 
+let parsedDarkMode = JSON.parse(storedDarkMode);
+let storedLightMode = localStorage.getItem('lightMode');
+let parsedLightMode = JSON.parse(storedLightMode); 
+*/
