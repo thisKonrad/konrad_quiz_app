5fb30af92c5a8548cfb8f89b567008f09fc2b4c4
this.Konrad <konradlebt@gmail.com>
2023-11-13 10:08:58 +0100

    select card by data attribute

--- JS/homeButtons.js
+++ JS/homeButtons.js
@@ -4,11 +4,20 @@ const bookmarkIcon = document.querySelector('[data-js="bookmark"]');
 const showAnswerBtn = document.querySelectorAll('.show_card_btn');
 const showQuestionBtn = document.querySelectorAll('.turn_card_btn');
 
+const card = document.querySelectorAll('[data-js="card"]');
+
 bookmarkIcon.addEventListener('click', toggleBookmarked);
-showAnswerBtn.addEventListener('click', toggleBookmarked);
-showQuestionBtn.addEventListener('click', toggleBookmarked);
+showAnswerBtn.addEventListener('click', showAnswer);
+showQuestionBtn.addEventListener('click', showQuestion);
 
 function toggleBookmarked(){
     console.log('clic')
     bookmarkIcon.classList.toggle('bookmarked');
 }
+
+function showAnswer(){
+    
+}
+
+
+function showQuestion(){}
