5af1a075c2307d9e1152d5b30a083dcc874d9187
this.Konrad <konradlebt@gmail.com>
2023-11-13 10:00:13 +0100

    toggle bookmark icon class and moved active bookmark class to css bookmark file

--- JS/homeButtons.js
+++ JS/homeButtons.js
@@ -1,2 +1,12 @@
 /* :: home buttons :: */
 
+const bookmarkIcon = document.querySelector('[data-js="bookmark"]');
+const showAnswerBtn = document.querySelectorAll('.show_card_btn');
+const showQuestionBtn = document.querySelectorAll('.turn_card_btn');
+
+bookmarkIcon.addEventListener('click', toggleBookmarked);
+
+function toggleBookmarked(){
+
+    bookmarkIcon.classList.toggle('.bookmarked');
+}
