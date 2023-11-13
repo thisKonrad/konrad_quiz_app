e9a355c80948e7e73b521c6a0b20610e54f2e0d2
this.Konrad <konradlebt@gmail.com>
2023-11-13 18:06:18 +0100

    solved event listeners on new generated cards

--- JS/form.js
+++ JS/form.js
@@ -1,8 +1,5 @@
 /* :::: form js :::: */
 
-/* const formData = new FormData(e.target);
-const data = Object.fromEntries(formData); */
-
 const cardContainer = document.querySelector('.new_cards_wrap');
 const form = document.querySelector('[data-js="question-form"]');
 const submitBtn = document.querySelector('[data-js="submit-button"]');
@@ -65,25 +62,6 @@ answerArea.addEventListener('keyup',()=>{
 });
 
 
-/* 
-<section class="card card_001" data-js="card">
-
-<div class="question" data-js="questionCard"> 
-    <img data-js="bookmark" class="question_bookmark" 
-    alt="bookmark_icon" src="./assets/bookmark.svg">
-    <h3>What does CSS stand for ?</h3>
-    <img name="css_html_icon" src="./assets/filetype-css.svg" 
-        alt="icon_css_file" class="language_icon">
-    <button class="show_card_btn">show answer</button>
-</div>
-
-<div class="answer" data-js="answerCard">
-    <button class="turn_card_btn">back</button>
-    <br>
-    <h3 class="answer_text">Cascading Style Sheets</h3>
-</div>
-</section> */ 
-
 function createNewCard(question, answer, tag){
 
     const cardSection = document.createElement('section');
@@ -91,17 +69,20 @@ function createNewCard(question, answer, tag){
 
     /* :::: new question card :::: */
     const questCardDiv = document.createElement('div');
+    questCardDiv.setAttribute('data-js','questionCard');
     questCardDiv.classList.add('question');
 
     const questCardIMG = document.createElement('img');
     questCardIMG.src = './assets/bookmark.svg';
     questCardIMG.classList.add('question_bookmark');
+    questCardIMG.setAttribute('data-js','bookmark');
 
     const questCardTitle = document.createElement('h3');
     questCardTitle.innerText = `${question}`;
 
-    const questionCardTag = document.createElement('p');
-    questionCardTag.innerText = `${tag}`;
+    const questCardTag = document.createElement('p');
+    questCardTag.innerText = `${tag}`;
+    questCardTag.classList.add('tag_style');
 
     const questCardBtn = document.createElement('button');
     questCardBtn.classList.add('show_card_btn');
@@ -110,9 +91,11 @@ function createNewCard(question, answer, tag){
     questCardDiv.append(questCardIMG);
     questCardDiv.append(questCardTitle);
     questCardDiv.append(questCardBtn);
+    questCardDiv.append(questCardTag);
 
     /* :::: new answer card face :::: */
     const answerCardDiv = document.createElement('div');
+    questCardDiv.setAttribute('data-js','answerCard');
     answerCardDiv.classList.add('answer');
 
     const answerCardBtn = document.createElement('button');
@@ -131,4 +114,49 @@ function createNewCard(question, answer, tag){
 
     cardContainer.appendChild(cardSection);
 
-}
+    /* :: controls :: */
+    questCardIMG.addEventListener('click', toggleBookmarked);
+    questCardBtn.addEventListener('click', showAnswer);
+    answerCardBtn.addEventListener('click', showQuestion);
+
+    function toggleBookmarked(){
+        console.log('clic')
+        questCardIMG.classList.toggle('bookmarked');
+    }
+
+    function showAnswer(){
+        console.log('ANS')
+        questCardDiv.classList.add('question--hide');
+        answerCardDiv.classList.add('answer--show');}
+
+
+    function showQuestion(){
+        console.log('QUES')
+        questCardDiv.classList.remove('question--hide');
+        answerCardDiv.classList.remove('answer--show');}
+
+};
+
+
+/* const questionCard = document.querySelector('[data-js="questionCard"]');
+const answerCard = document.querySelector('[data-js="answerCard"]'); */
+
+
+/* 
+<section class="card card_001" data-js="card">
+
+<div class="question" data-js="questionCard"> 
+    <img data-js="bookmark" class="question_bookmark" 
+    alt="bookmark_icon" src="./assets/bookmark.svg">
+    <h3>What does CSS stand for ?</h3>
+    <img name="css_html_icon" src="./assets/filetype-css.svg" 
+        alt="icon_css_file" class="language_icon">
+    <button class="show_card_btn">show answer</button>
+</div>
+
+<div class="answer" data-js="answerCard">
+    <button class="turn_card_btn">back</button>
+    <br>
+    <h3 class="answer_text">Cascading Style Sheets</h3>
+</div>
+</section> */ 
