81ef831da6a5d97d28fe570b9ffa8ab8d891aa5a
this.Konrad <konradlebt@gmail.com>
2023-11-14 09:42:19 +0100

    clean up js files

--- JS/form.js
+++ JS/form.js
@@ -1,22 +1,15 @@
 /* :::: form js :::: */
-
 const cardContainer = document.querySelector('.new_cards_wrap');
 const form = document.querySelector('[data-js="question-form"]');
 const submitBtn = document.querySelector('[data-js="submit-button"]');
 
+
 submitBtn.addEventListener('click',(e)=>{
 
     e.preventDefault();
-    console.log('clicked')
-
-    console.log("formbtn: ",submitBtn)
-
-    console.log("Event: ",e.target)
 
     const formData = new FormData(form);
     const data = Object.fromEntries(formData); 
-
-    console.log("data: ",data);
 
     const formQuestion = data['text-question'];
     const formAnswer = data['text-answer'];
@@ -24,12 +17,6 @@ submitBtn.addEventListener('click',(e)=>{
 
     const questionLength = formQuestion.length;
     const answerLength = formQuestion.length;
-
-    console.log("length of answer:", answerLength)
-    console.log("length of question:", questionLength)
-    console.log("data Ques:", formQuestion);
-    console.log("dataAnswer: ", formAnswer);
-    console.log("dataTag: ", formTag); 
 
     createNewCard(formQuestion, formAnswer, formTag);
 
@@ -49,7 +36,6 @@ questionArea.addEventListener('keyup',()=>{
 
     const quesCharValue = questionArea.value.length;
     const quesCharsLeft = maxQuestionCharCount - quesCharValue;
-
     questCharCount.innerText = `${quesCharsLeft}`;
 
 });
@@ -59,7 +45,6 @@ answerArea.addEventListener('keyup',()=>{
 
     const ansCharValue = answerArea.value.length;
     const ansCharsLeft = maxAnswerCharCount - ansCharValue;
-
     answerCharCount.innerText = `${ansCharsLeft}`;
 
 });
@@ -123,43 +108,16 @@ function createNewCard(question, answer, tag){
     answerCardBtn.addEventListener('click', showQuestion);
 
     function toggleBookmarked(){
-        console.log('clic')
         questCardIMG.classList.toggle('bookmarked');
     }
 
     function showAnswer(){
-        console.log('ANS')
         questCardDiv.classList.add('question--hide');
         answerCardDiv.classList.add('answer--show');}
 
 
     function showQuestion(){
-        console.log('QUES')
         questCardDiv.classList.remove('question--hide');
         answerCardDiv.classList.remove('answer--show');}
 
 };
-
-
-/* const questionCard = document.querySelector('[data-js="questionCard"]');
-const answerCard = document.querySelector('[data-js="answerCard"]'); */
-
-
-/* 
-<section class="card card_001" data-js="card">
-
-<div class="question" data-js="questionCard"> 
-    <img data-js="bookmark" class="question_bookmark" 
-    alt="bookmark_icon" src="./assets/bookmark.svg">
-    <h3>What does CSS stand for ?</h3>
-    <img name="css_html_icon" src="./assets/filetype-css.svg" 
-        alt="icon_css_file" class="language_icon">
-    <button class="show_card_btn">show answer</button>
-</div>
-
-<div class="answer" data-js="answerCard">
-    <button class="turn_card_btn">back</button>
-    <br>
-    <h3 class="answer_text">Cascading Style Sheets</h3>
-</div>
-</section> */ 
